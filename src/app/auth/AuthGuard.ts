d0b7dcdd8b7c4730decc64a1d6c03a51acb4e874
Md. Rafiqul Islam <Rafiq.echoece@gmail.com>
2025-04-13 22:20:49 +0600

    initial commit

---
+++ src/app/auth/AuthGuard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    GuardResult,
+    MaybeAsync,
+    Router,
+    RouterStateSnapshot
+} from '@angular/router';
+import { AuthService } from './auth.service';
+import { take } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+
+        return this.authService.user$.pipe(
+            take(1), // Take the latest user value and complete
+            map(user => {
+                const isAuthenticated = !!user;
+
+                if (isAuthenticated) {
+                    return true; // Allow access
+                }
+
+                // Redirect to login page with return URL
+                return this.router.createUrlTree(
+                    ['/auth/login'],
+                    { queryParams: { returnUrl: state.url } }
+                );
+            })
+        );
+    }
+
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://localhost:8091/',
+    urlVersion: 'api/v1',
+}
